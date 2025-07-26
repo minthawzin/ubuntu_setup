94be96872f104da0c89db1248f0269326091bf34
MinThawZin <minthawzin@globalwalkers.co.jp>
2025-07-26 14:30:53 +0630

    [update] auto download debs and install

--- install_apps.sh
+++ install_apps.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Define the ZIP file and destination folder
+ZIP_FILE="debs.zip"
+DEST_FOLDER="./"
+
+wget -c https://github.com/minthawzin/ubuntu_setup/releases/download/v0.0.1/debs.zip
+# Create the destination folder if it doesn't exist
+mkdir -p "$DEST_FOLDER"
+
+# Extract the ZIP file
+unzip "$ZIP_FILE" -d "$DEST_FOLDER"
+
 sudo dpkg -i debs/anydesk_7.0.1-1_amd64.deb
 sudo dpkg -i debs/code_1.102.2-1753187809_amd64.deb
 sudo dpkg -i debs/slack-desktop-4.45.64-amd64.deb
